fcaec17e648ac5098d84c5b0c2731d83130dd65f
Berno Kleinhans <bb@ii.coop>
2020-08-10 19:23:54 +0000

    Change product yaml diff comment

--- plugin/plugin.go
+++ plugin/plugin.go
@@ -252,7 +252,7 @@ func HandleAll(log *logrus.Entry, ghc githubClient, config *plugins.Configuratio
 						prLogger.Infof("pYC in HANDLEALL productYamlCorrect returned %v\n",productYamlCorrect)
 
 						githubClient.CreateComment(ghc, org, repo, prNumber, "This request is not yet verifiable, please confirm that your product file ( PRODUCT.yaml ) is named correctly and have all the fields listed in  [How to submit conformance results](https://github.com/cncf/k8s-conformance/blob/master/instructions.md#productyaml) .")
-						githubClient.AddLabel(ghc, org, repo, prNumber, "You are missing the following fields"+prodYamlDiffString)
+						githubClient.CreateComment(ghc, org, repo, prNumber, "You are missing the following fields"+prodYamlDiffString)
 						if !hasNotVerifiableLabel {
 							githubClient.AddLabel(ghc, org, repo, prNumber, "not-verifiable")
 						}

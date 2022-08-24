31111b54c76ef8f83d162bc8f2f11ecd6723f908
Caleb Woodbine <calebwoodbine.public@gmail.com>
2022-08-24 15:42:15 +1200

    Add generate conformanceyaml

---
+++ hack/generate-conformanceyaml.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+# Prepare conformance metadata
+
+cd "$(git rev-parse --show-toplevel)"
+
+K8S_LATEST_VERSION=$(curl -L -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)
+K8S_LATEST_MINOR_VERSION="$(awk '{split($1,array, "."); print array[2]}' <<< $K8S_LATEST_VERSION)"
+SETS=($(seq 19 $K8S_LATEST_MINOR_VERSION))
+rm -r ./kodata/conformance-testdata/
+
+MANIFESTS=(
+  https://raw.githubusercontent.com/cncf-infra/prow-config/master/docs/conformance_v1.{15..17}.yaml
+  https://raw.githubusercontent.com/cncf-infra/prow-config/master/tests/conformance-1.18.yaml
+  https://raw.githubusercontent.com/kubernetes/kubernetes/master/test/conformance/testdata/conformance.yaml
+)
+for SET in "${SETS[@]}"; do
+  MANIFESTS+=("https://raw.githubusercontent.com/kubernetes/kubernetes/release-1.$SET/test/conformance/testdata/conformance.yaml")
+done
+
+re="^.*([0-9].[0-9]{2}|master).*$"
+for METADATA in ${MANIFESTS[*]}; do
+    if [[ $METADATA =~ $re ]]; then
+        version=${BASH_REMATCH[1]}
+
+        semver="v${version}"
+        if [ "${version}" = master ]; then
+            semver="${version}"
+        fi
+        mkdir -p ./kodata/conformance-testdata/$semver/
+        curl -L \
+            -s \
+            -o ./kodata/conformance-testdata/${semver}/conformance.yaml \
+            ${METADATA}
+    fi
+done

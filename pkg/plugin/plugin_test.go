9b11aeccf3ce3b94fb168a2da67f6bdc292cafe5
Caleb Woodbine <calebwoodbine.public@gmail.com>
2023-08-13 14:40:05 +1200

    feat: add plugin test for NewPRSuiteForPR
    add important test

--- pkg/plugin/plugin_test.go
+++ pkg/plugin/plugin_test.go
@@ -1,15 +1,212 @@
 package plugin
 
 import (
+	"context"
+	_ "embed"
+	"fmt"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 
 	githubql "github.com/shurcooL/githubv4"
+	"github.com/sirupsen/logrus"
 	"k8s.io/test-infra/prow/config"
+	"k8s.io/test-infra/prow/github"
+	"sigs.k8s.io/yaml"
 
+	"cncf.io/infra/verify-conformance-release/pkg/common"
 	"cncf.io/infra/verify-conformance-release/pkg/suite"
 )
+
+var (
+	log = logrus.StandardLogger().WithField("plugin", "verify-conformance-release")
+
+	//go:embed testdata/TestGetJunitSubmittedConformanceTests-coolkube-v1-27-junit_01.xml
+	testGetJunitSubmittedConformanceTestsCoolkubeV127Junit_01xml string
+)
+
+type prContext struct {
+	PullRequestQuery *suite.PullRequestQuery
+	SupportingFiles  []*suite.PullRequestFile
+}
+
+type FakeGitHubClient struct {
+	PopulatedPullRequests []*prContext
+}
+
+func NewFakeGitHubClient(p []*prContext) *FakeGitHubClient {
+	return &FakeGitHubClient{
+		PopulatedPullRequests: p,
+	}
+}
+
+func (f *FakeGitHubClient) CreateStatus(org string, repo string, headRefOID string, status github.Status) error {
+	return nil
+}
+func (f *FakeGitHubClient) GetCombinedStatus(org, repo, ref string) (*github.CombinedStatus, error) {
+	return nil, nil
+}
+func (f *FakeGitHubClient) GetIssueLabels(org, repo string, number int) ([]github.Label, error) {
+	labels := []github.Label{}
+	var prIndex *int
+	for i := range f.PopulatedPullRequests {
+		if f.PopulatedPullRequests[i].PullRequestQuery.Number == githubql.Int(number) {
+			prIndex = &i
+		}
+	}
+	if prIndex == nil {
+		return []github.Label{}, fmt.Errorf("unable to find pr '%v'", number)
+	}
+	for _, l := range f.PopulatedPullRequests[*prIndex].PullRequestQuery.Labels.Nodes {
+		labels = append(labels, github.Label{Name: string(l.Name)})
+	}
+	return labels, nil
+}
+func (f *FakeGitHubClient) CreateComment(org, repo string, number int, comment string) error {
+	return nil
+}
+func (f *FakeGitHubClient) ListIssueCommentsWithContext(ctx context.Context, org, repo string, number int) ([]github.IssueComment, error) {
+	return []github.IssueComment{}, nil
+}
+func (f *FakeGitHubClient) BotUserChecker() (func(candidate string) bool, error) {
+	return func(string) bool { return false }, nil
+}
+func (f *FakeGitHubClient) AddLabel(org, repo string, number int, label string) error {
+	var prIndex *int
+	for i := range f.PopulatedPullRequests {
+		if f.PopulatedPullRequests[i].PullRequestQuery.Number == githubql.Int(number) {
+			prIndex = &i
+		}
+	}
+	if prIndex == nil {
+		return fmt.Errorf("unable to find label '%v' in pr number '%v'", label, number)
+	}
+	f.PopulatedPullRequests[*prIndex].PullRequestQuery.Labels.Nodes = append(f.PopulatedPullRequests[*prIndex].PullRequestQuery.Labels.Nodes, struct{ Name githubql.String }{githubql.String(label)})
+	return nil
+}
+func (f *FakeGitHubClient) RemoveLabel(org, repo string, number int, label string) error {
+	var prIndex *int
+	for i := range f.PopulatedPullRequests {
+		if f.PopulatedPullRequests[i].PullRequestQuery.Number == githubql.Int(number) {
+			prIndex = &i
+		}
+	}
+	if prIndex == nil {
+		return fmt.Errorf("unable to find label '%v' in pr number '%v'", label, number)
+	}
+	var labelIndex *int
+	for i, l := range f.PopulatedPullRequests[*prIndex].PullRequestQuery.Labels.Nodes {
+		if l.Name == githubql.String(label) {
+			labelIndex = &i
+		}
+	}
+	if labelIndex == nil {
+		return fmt.Errorf("unable to find label '%v' in pr number '%v'", label, number)
+	}
+	f.PopulatedPullRequests[*prIndex].PullRequestQuery.Labels.Nodes = append(f.PopulatedPullRequests[*prIndex].PullRequestQuery.Labels.Nodes[:*labelIndex], f.PopulatedPullRequests[*prIndex].PullRequestQuery.Labels.Nodes[*labelIndex+1:]...)
+	return nil
+}
+func (f *FakeGitHubClient) DeleteStaleComments(org, repo string, number int, comments []github.IssueComment, isStale func(github.IssueComment) bool) error {
+	return nil
+}
+func (f *FakeGitHubClient) QueryWithGitHubAppsSupport(ctx context.Context, sq interface{}, vars map[string]interface{}, org string) error {
+	if org == "nil" {
+		return fmt.Errorf("org does not exist")
+	}
+	// wrap each pull request in an array struct, as per search query nodes
+	if len(f.PopulatedPullRequests) > 0 && f.PopulatedPullRequests[0] == nil {
+		return fmt.Errorf("empty pr")
+	}
+	nodes := func() []struct {
+		PullRequest suite.PullRequestQuery "graphql:\"... on PullRequest\""
+	} {
+		o := []struct {
+			PullRequest suite.PullRequestQuery "graphql:\"... on PullRequest\""
+		}{}
+		for _, pr := range f.PopulatedPullRequests {
+			if pr.PullRequestQuery == nil {
+				continue
+			}
+			o = append(o, struct {
+				PullRequest suite.PullRequestQuery "graphql:\"... on PullRequest\""
+			}{
+				PullRequest: *pr.PullRequestQuery,
+			})
+		}
+		return o
+	}()
+	query, ok := sq.(*SearchQuery)
+	if !ok {
+		return fmt.Errorf("failed to case sq to SearchQuery")
+	}
+	hasNextPage := false
+	// TODO tidy this
+	searchCursor := func() string {
+		s := vars["searchCursor"].(*githubql.String)
+		if s == nil {
+			return "1"
+		}
+		return string(*s)
+	}()
+	if searchCursor == "1" {
+		hasNextPage = true
+		searchCursor = "2"
+	} else if searchCursor == "2" {
+		hasNextPage = false
+		searchCursor = "3"
+	} else {
+		searchCursor = "1"
+	}
+	*query = SearchQuery{
+		RateLimit: struct {
+			Cost      githubql.Int
+			Remaining githubql.Int
+		}{
+			Cost:      githubql.Int(1),
+			Remaining: githubql.Int(999999),
+		},
+		Search: struct {
+			PageInfo struct {
+				HasNextPage githubql.Boolean
+				EndCursor   githubql.String
+			}
+			Nodes []struct {
+				PullRequest suite.PullRequestQuery "graphql:\"... on PullRequest\""
+			}
+		}{
+			PageInfo: struct {
+				HasNextPage githubql.Boolean
+				EndCursor   githubql.String
+			}{
+				HasNextPage: githubql.Boolean(hasNextPage),
+				EndCursor:   githubql.String(searchCursor),
+			},
+			Nodes: nodes,
+		},
+	}
+	return nil
+}
+func (f *FakeGitHubClient) GetPullRequest(org, repo string, number int) (*github.PullRequest, error) {
+	return nil, nil
+}
+func (f *FakeGitHubClient) GetPullRequestChanges(org, repo string, number int) ([]github.PullRequestChange, error) {
+	pr := &prContext{}
+	prChanges := []github.PullRequestChange{}
+	for _, n := range f.PopulatedPullRequests {
+		if n.PullRequestQuery.Number == githubql.Int(number) {
+			pr = n
+			break
+		}
+	}
+	for _, file := range pr.SupportingFiles {
+		prChanges = append(prChanges, github.PullRequestChange{
+			Filename: file.Name,
+			BlobURL:  file.BlobURL,
+		})
+	}
+	return prChanges, nil
+}
 
 func TestHelpProvider(t *testing.T) {
 	hp, err := HelpProvider([]config.OrgRepo{})
@@ -40,6 +237,7 @@ func TestFetchFileFromURI(t *testing.T) {
 	if resp.StatusCode != http.StatusOK {
 		t.Fatalf("error: response code doesn't match what was expected")
 	}
+
 }
 
 func TestRawURLForBlobURL(t *testing.T) {
@@ -69,11 +267,194 @@ func TestRawURLForBlobURL(t *testing.T) {
 }
 
 func TestSearch(t *testing.T) {
-
+	type testCase struct {
+		Name                string
+		PullRequestQuery    *suite.PullRequestQuery
+		ExpectedErrorString string
+	}
+	for _, tc := range []testCase{
+		{
+			Name: "complete result",
+			PullRequestQuery: &suite.PullRequestQuery{
+				Number: githubql.Int(1),
+				Author: struct{ Login githubql.String }{
+					Login: githubql.String("cncf"),
+				},
+				Repository: struct {
+					Name  githubql.String
+					Owner struct{ Login githubql.String }
+				}{
+					Name: githubql.String("k8s-conformance"),
+					Owner: struct{ Login githubql.String }{
+						Login: githubql.String("cncf"),
+					},
+				},
+			},
+		},
+		{
+			Name: "org does not exist",
+			PullRequestQuery: &suite.PullRequestQuery{
+				Number: githubql.Int(1),
+				Author: struct{ Login githubql.String }{
+					Login: githubql.String("nil"),
+				},
+				Repository: struct {
+					Name  githubql.String
+					Owner struct{ Login githubql.String }
+				}{
+					Name: githubql.String("k8s-conformance"),
+					Owner: struct{ Login githubql.String }{
+						Login: githubql.String("nil"),
+					},
+				},
+			},
+			ExpectedErrorString: "org does not exist",
+		},
+		{
+			Name:                "empty",
+			PullRequestQuery:    nil,
+			ExpectedErrorString: "empty pr",
+		},
+	} {
+		ghc := NewFakeGitHubClient([]*prContext{
+			{
+				PullRequestQuery: tc.PullRequestQuery,
+			},
+		})
+		var org string
+		if tc.PullRequestQuery != nil {
+			org = string(tc.PullRequestQuery.Repository.Owner.Login)
+		}
+		prs, err := search(context.TODO(), log, ghc, "archived:false is:pr is:open repo:\"k8s-conformance\"", org)
+		t.Logf("tc(%v) has error %v", tc.Name, err != nil)
+		if err != nil && !strings.Contains(err.Error(), tc.ExpectedErrorString) {
+			t.Fatalf("error searching for PRs: %v", err)
+		}
+		t.Logf("tc(%v) %+v\n", tc.Name, prs)
+	}
 }
 
 func TestNewPRSuiteForPR(t *testing.T) {
+	type testCase struct {
+		Name                string
+		PullRequestQuery    *suite.PullRequestQuery
+		Labes               []github.Label
+		SupportingFiles     []*suite.PullRequestFile
+		ExpectedErrorString string
+	}
 
+	common.DataPathPrefix = "../../"
+
+	for _, tc := range []testCase{
+		{
+			Name: "valid pull request entry",
+			Labes: []github.Label{
+				{
+					Name: "conformance-product-submission",
+				},
+			},
+			PullRequestQuery: &suite.PullRequestQuery{
+				Number: githubql.Int(1),
+				Repository: struct {
+					Name  githubql.String
+					Owner struct{ Login githubql.String }
+				}{
+					Name: githubql.String("cncf-ci"),
+					Owner: struct{ Login githubql.String }{
+						Login: githubql.String("cncf-ci"),
+					},
+				},
+			},
+			SupportingFiles: []*suite.PullRequestFile{
+				{
+					Name:     "v1.27/coolkube/README.md",
+					BaseName: "README.md",
+					BlobURL:  "README.md",
+					Contents: `# CoolKube`,
+				},
+				{
+					Name:     "v1.27/coolkube/PRODUCT.yaml",
+					BaseName: "PRODUCT.yaml",
+					BlobURL:  "PRODUCT.yaml",
+					Contents: `vendor: "cool"
+name: "coolkube"
+version: "v1.27"
+type: "distribution"
+description: "it's just all-round cool and probably the best k8s, idk"
+website_url: "website_url"
+documentation_url: "docs"
+contact_email_address: "sales@coolkubernetes.com"`,
+				},
+				{
+					Name:     "v1.27/coolkube/junit_01.xml",
+					BaseName: "junit_01.xml",
+					BlobURL:  "junit_01.xml",
+					Contents: testGetJunitSubmittedConformanceTestsCoolkubeV127Junit_01xml,
+				},
+				{
+					Name:     "v1.27/coolkube/e2e.log",
+					BaseName: "e2e.log",
+					BlobURL:  "e2e.log",
+					Contents: `cool!`,
+				},
+			},
+		},
+	} {
+		productYAML := map[string]string{}
+		var productYAMLSupportingFile string
+		for _, file := range tc.SupportingFiles {
+			if file.BaseName == "PRODUCT.yaml" {
+				productYAMLSupportingFile = file.Contents
+			}
+		}
+		if productYAMLSupportingFile != "" {
+			if err := yaml.Unmarshal([]byte(productYAMLSupportingFile), &productYAML); err != nil {
+				t.Fatalf("error: unmarshalling from PRODUCT.yaml supporting file: %v", err)
+			}
+		}
+
+		svr := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			t.Logf("requesting path '%v'", r.URL.Path)
+			supportingFile := &suite.PullRequestFile{}
+			for _, file := range tc.SupportingFiles {
+				if r.URL.Path == "/"+file.BaseName || r.URL.Path == "/"+file.Name {
+					supportingFile = file
+				}
+			}
+			w.WriteHeader(http.StatusOK)
+			_, err := w.Write([]byte(supportingFile.Contents))
+			if err != nil {
+				t.Fatalf("error: sending http response; %v", err)
+			}
+		}))
+		defer svr.Close()
+		for _, field := range []string{"website_url", "documentation_url"} {
+			if productYAML[field] != "" {
+				productYAML[field] = svr.URL + "/" + productYAML[field]
+			}
+		}
+		productYAMLBytes, err := yaml.Marshal(productYAML)
+		if err != nil {
+			t.Fatalf("error: marshalling new product yaml: %v", err)
+		}
+		for i := range tc.SupportingFiles {
+			tc.SupportingFiles[i].BlobURL = svr.URL + "/" + tc.SupportingFiles[i].BlobURL
+			if tc.SupportingFiles[i].BaseName == "PRODUCT.yaml" {
+				tc.SupportingFiles[i].Contents = string(productYAMLBytes)
+			}
+		}
+		ghc := NewFakeGitHubClient([]*prContext{
+			{
+				PullRequestQuery: tc.PullRequestQuery,
+				SupportingFiles:  tc.SupportingFiles,
+			},
+		})
+		prSuite, err := NewPRSuiteForPR(log, ghc, tc.PullRequestQuery)
+		if err != nil && strings.Contains(err.Error(), tc.ExpectedErrorString) {
+			t.Fatalf("unexpected error in testcase '%v': %v", tc.Name, err)
+		}
+		t.Logf("prSuite: %+v\n", prSuite)
+	}
 }
 
 func TestGetGodogPaths(t *testing.T) {
@@ -323,11 +704,192 @@ func TestUpdateStatus(t *testing.T) {
 }
 
 func TestHandle(t *testing.T) {
+	type testCase struct {
+		Name                    string
+		KubernetesVersion       *string
+		KubernetesVersionLatest *string
+		PullRequestQuery        *suite.PullRequestQuery
+		SupportingFiles         []*suite.PullRequestFile
+		Labels                  []string
+		ExpectedLabels          []string
+		ExpectedComment         string
+		ExpectedStatus          string
+		ExpectedError           string
+	}
 
+	for _, tc := range []testCase{
+		{
+			Name:                    "valid submission",
+			Labels:                  []string{"conformance-product-submission"},
+			KubernetesVersion:       common.Pointer("v1.27"),
+			KubernetesVersionLatest: common.Pointer("v1.27"),
+			SupportingFiles: []*suite.PullRequestFile{
+				{
+					Name:     "v1.27/coolkube/README.md",
+					BaseName: "README.md",
+					Contents: `# coolkube
+> the coolest Kubernetes distribution
+
+## Generating conformance results
+
+1. create a coolkube cluster
+2. sonobuoy run --wait && sonobuoy results "$(sonobuoy retrieve)" && sonobuoy delete --wait`,
+					BlobURL: "README.md",
+				},
+				{
+					Name:     "v1.27/coolkube/PRODUCT.yaml",
+					BaseName: "PRODUCT.yaml",
+					Contents: `vendor: "cool"
+name: "coolkube"
+version: "v1.27"
+type: "distribution"
+description: "it's just all-round cool and probably the best k8s, idk"
+website_url: "website_url"
+documentation_url: "docs"
+contact_email_address: "sales@coolkubernetes.com"`,
+					BlobURL: "PRODUCT.yaml",
+				},
+				{
+					Name:     "v1.27/coolkube/e2e.log",
+					BaseName: "e2e.log",
+					Contents: "",
+					BlobURL:  "e2e.log",
+				},
+				{
+					Name:     "v1.27/coolkube/junit_01.xml",
+					BaseName: "junit_01.xml",
+					Contents: testGetJunitSubmittedConformanceTestsCoolkubeV127Junit_01xml,
+					BlobURL:  "junit_01.xml",
+				},
+			},
+			PullRequestQuery: &suite.PullRequestQuery{
+				Title: githubql.String("Conformance results for v1.27/coolkube"),
+				Commits: struct {
+					Nodes []struct {
+						Commit struct {
+							Oid    githubql.String
+							Status struct {
+								Contexts []struct {
+									Context githubql.String
+									State   githubql.String
+								}
+							}
+						}
+					}
+				}{
+					Nodes: []struct {
+						Commit struct {
+							Oid    githubql.String
+							Status struct {
+								Contexts []struct {
+									Context githubql.String
+									State   githubql.String
+								}
+							}
+						}
+					}{
+						{
+							Commit: struct {
+								Oid    githubql.String
+								Status struct {
+									Contexts []struct {
+										Context githubql.String
+										State   githubql.String
+									}
+								}
+							}{
+								Oid: githubql.String(""),
+								Status: struct {
+									Contexts []struct {
+										Context githubql.String
+										State   githubql.String
+									}
+								}{
+									Contexts: []struct {
+										Context githubql.String
+										State   githubql.String
+									}{
+										{
+											Context: githubql.String(""),
+											State:   githubql.String(""),
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	} {
+		productYAML := map[string]string{}
+		var productYAMLSupportingFile string
+		for _, file := range tc.SupportingFiles {
+			if file.BaseName == "PRODUCT.yaml" {
+				productYAMLSupportingFile = file.Contents
+			}
+		}
+		if productYAMLSupportingFile != "" {
+			if err := yaml.Unmarshal([]byte(productYAMLSupportingFile), &productYAML); err != nil {
+				t.Fatalf("error: unmarshalling from PRODUCT.yaml supporting file: %v", err)
+			}
+		}
+		svr := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			t.Logf("requesting path '%v'", r.URL.Path)
+			supportingFile := &suite.PullRequestFile{}
+			for _, file := range tc.SupportingFiles {
+				if r.URL.Path == "/"+file.BaseName || r.URL.Path == "/"+file.Name {
+					supportingFile = file
+				}
+			}
+			w.WriteHeader(http.StatusOK)
+			_, err := w.Write([]byte(supportingFile.Contents))
+			if err != nil {
+				t.Fatalf("error: sending http response; %v", err)
+			}
+		}))
+		defer svr.Close()
+		for _, field := range []string{"website_url", "documentation_url"} {
+			if productYAML[field] != "" {
+				productYAML[field] = svr.URL + "/" + productYAML[field]
+			}
+		}
+		productYAMLBytes, err := yaml.Marshal(productYAML)
+		if err != nil {
+			t.Fatalf("error: marshalling new product yaml: %v", err)
+		}
+		for i := range tc.SupportingFiles {
+			tc.SupportingFiles[i].BlobURL = svr.URL + "/" + tc.SupportingFiles[i].BlobURL
+			if tc.SupportingFiles[i].BaseName == "PRODUCT.yaml" {
+				tc.SupportingFiles[i].Contents = string(productYAMLBytes)
+			}
+		}
+		ghc := NewFakeGitHubClient([]*prContext{
+			{
+				PullRequestQuery: tc.PullRequestQuery,
+				SupportingFiles:  tc.SupportingFiles,
+			},
+		})
+		if err := handle(log, ghc, tc.PullRequestQuery); err != nil {
+			t.Fatalf("unexpected error: %v", err)
+		}
+		// TODO check comment, labels and status
+	}
 }
 
 func TestNewPullRequestQueryForGithubPullRequest(t *testing.T) {
-
+	if prq := NewPullRequestQueryForGithubPullRequest(
+		"cncf",
+		"k8s-conformance",
+		0,
+		&github.PullRequest{
+			User: github.User{
+				Login: "cncf-ci",
+			},
+		},
+	); prq == nil {
+		t.Fatalf("PullRequestQuery must never be empty")
+	}
 }
 
 func TestHandlePullRequestEvent(t *testing.T) {
